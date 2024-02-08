b6275ca3a992ed3e2e2757df19a3d208eece361d
Jose Sanchez <josemsanchezq00@gmail.com>
2024-02-07 17:37:23 -0700

    added test scripts AAA

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const tasksRepository = require("./tasksRespository");
 
 const app = express();
 const port = 3000;
@@ -13,13 +14,14 @@ let tasks = [
 
 // Get all tasks
 app.get('/tasks', (req, res) => {
+  const tasks = tasksRepository.getAll();
   res.json(tasks);
 });
 
 // Get a specific task
 app.get('/tasks/:id', (req, res) => {
   const taskId = parseInt(req.params.id);
-  const task = tasks.find((t) => t.id === taskId);
+  const task = tasksRepository.getById(taskId);
 
   if (task) {
     res.json(task);
@@ -31,8 +33,7 @@ app.get('/tasks/:id', (req, res) => {
 // Create a new task
 app.post('/tasks', (req, res) => {
   const newTask = req.body;
-  newTask.id = tasks.length + 1;
-  tasks.push(newTask);
+  tasksRepository.createTask(newTask)
   res.status(201).json(newTask);
 });
 
@@ -40,11 +41,10 @@ app.post('/tasks', (req, res) => {
 app.put('/tasks/:id', (req, res) => {
   const taskId = parseInt(req.params.id);
   const updatedTask = req.body;
-  const index = tasks.findIndex((t) => t.id === taskId);
+  const task = tasksRepository.updateTask(taskId, updatedTask);
 
-  if (index !== -1) {
-    tasks[index] = { ...tasks[index], ...updatedTask };
-    res.json(tasks[index]);
+  if (task != null) {
+    res.json(task);
   } else {
     res.status(404).json({ error: 'Task not found' });
   }
@@ -53,7 +53,7 @@ app.put('/tasks/:id', (req, res) => {
 // Delete a task
 app.delete('/tasks/:id', (req, res) => {
   const taskId = parseInt(req.params.id);
-  tasks = tasks.filter((t) => t.id !== taskId);
+  tasksRepository.deleteTask(taskId);
   res.sendStatus(204);
 });
 
